b270408ea412e8da5bd944f4c1cf32b93225c13b
vukmilenkovic <vukmilenkovic9@gmail.com>
2025-12-04 11:48:16 +0100

    Added variables and created a function that takes two number arguments, and an operator and calls the correct function depending on the operator when invoked.

--- index.js
+++ index.js
@@ -14,3 +14,24 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+// Variables
+let a;
+let b;
+let operator;
+
+// Function operator
+function operate(a, b, operator){
+    if (operator === '-'){
+        return subtract(a, b);
+    }
+    if (operator === '+'){
+        return add(a, b);
+    }
+    if (operator === '*'){
+        return multiply(a, b);
+    }
+    if (operator === '/'){
+        return divide(a, b);
+    }
+}
